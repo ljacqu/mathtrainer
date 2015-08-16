0a087b7a36ebc1a7b1be3e9614344f5ed814b948
ljacqu <ljacqu@users.noreply.github.com>
2015-08-16 14:26:41 +0200

    Refactor: all timer logic in object modules

--- trainer.js
+++ trainer.js
@@ -1,16 +1,25 @@
 (function () {
   'use strict';
 
-  // ================
-  // Trainer variables
-  // ================
+  /**
+   * List of supported operators.
+   */
   var operators = {
     add: {name: 'add', sign: '+',    result: function (a, b) { return a + b; }},
     sub: {name: 'sub', sign: '-',    result: function (a, b) { return a - b; }},
     mul: {name: 'mul', sign: '\xD7', result: function (a, b) { return a * b; }},
     div: {name: 'div', sign: '\xF7', result: function (a, b) { return a / b; }}
   };
-  
+
+  /**
+   * Configuration parameters provided by the user.
+   *  min: The smallest number to use in the questions.
+   *  max: The largest number to use in the questions.
+   *  minutes: The number of minutes the trainer should run for.
+   *  avoidNegatives: If true, guarantees that subtraction results are never 
+   *     negative. Only takes effect if min >= 0.
+   *  operators: The operators to use in the questions.
+   */
   var config = {
     min: 0,
     max: 0,
@@ -18,81 +27,121 @@
     avoidNegatives: true,
     operators: ['add', 'div']
   };
-  
+
+  /**
+   * Statistics of the current run.
+   */
   var stats = {
     total: 0,
     skipped: 0
   };
-  
+
+  /**
+   * Data of the current question the trainer is showing.
+   */
   var question = {
     a: 0,
     b: 0,
     result: 0
   };
 
+  /** Trainer logic. */
   var trainer = {};
-  // ================
-  // Trainer functions
-  // ================
-  /**
-   * Checks if the result in the field is correct and creates a new question if
-   * this is the case.
-   */
-  function checkUserSubmission() {
-    var userResult = parseInt($('#result').val(), 10);
-    if (!isNaN(userResult) && userResult === question.result) {
-      ++stats.total;
-      createNewQuestion();
-    }
-  }
-  /**
-   * Allows a question to be skipped; a new question will be created.
-   */
-  function skipQuestion() {
-    ++stats.skipped;
-    createNewQuestion();
-  }
-  /**
-   * Creates a new question (updates the variables accordingly).
-   */
-  function createNewQuestion() {
-    var a = randomInt(config.min, config.max);
-    var b = randomInt(config.min, config.max);
 
-    var operator = getRandomOperator();
-    if (operator.name === 'sub' && config.avoidNegatives) {
-      var ab = swapBigger(a, b);
-      a = ab[1];
-      b = ab[0];
-      question.result = a - b;
-    } else if (operator.name === 'div') {
-      var mulResult = a * b;
-      question.result = a;
-      a = mulResult;
-    } else {
-      question.result = operator.result(a, b);
-    }
-    question.a = a;
-    question.b = b;
-    updateQuestionText(operator.sign);
-  }
-  function updateQuestionText(sign) {
-    $('#question_math').text(question.a + '\xA0' + sign + '\xA0' + question.b);
-    $('#result').val('');
-    $('#progress').text(stats.total + ' answered, ' + stats.skipped + 
-      ' skipped');
-  }
-  function getRandomOperator() {
-    return operators[
-      config.operators[randomInt(0, config.operators.length - 1)]
-    ];
-  }
+  /**
+   * Functions for the current trainer question.
+   */
+  trainer.question = function () {
+    /**
+     * Returns a random integer between min and max.
+     * @param {Number} min lower bound
+     * @param {Number} max upper bound
+     * @returns {Number} Random int in [min, max]
+     */
+    var randomInt = function (min, max) {
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    };
 
-  // ================
-  // Functions for options
-  // ================
+    /**
+     * Returns a random operator the user wants to use.
+     * @returns {operators}
+     */
+    var getRandomOperator = function () {
+      return operators[
+        config.operators[randomInt(0, config.operators.length - 1)]
+      ];
+    };
+
+    /**
+     * Updates the HTML document with the data from the question.
+     * @param {String} sign The sign symbol of the operator used in the question
+     */
+    var updateQuestionText = function (sign) {
+      $('#result').val('');
+      $('#question_math').text(question.a + '\xA0' + sign + '\xA0' +
+        question.b);
+      $('#progress').text(stats.total + ' answered, ' + stats.skipped +
+        ' skipped');
+    };
+
+    /**
+     * Creates a new question (updates the variables accordingly).
+     */
+    var createNew = function () {
+      var a = randomInt(config.min, config.max);
+      var b = randomInt(config.min, config.max);
+
+      var operator = getRandomOperator();
+      if (operator.name === 'sub' && config.avoidNegatives) {
+        // Ensure that a <= b when config.avoidNegatives is true
+        var ab = a > b ? [b, a] : [a, b];
+        a = ab[1];
+        b = ab[0];
+        question.result = a - b;
+      } else if (operator.name === 'div') {
+        var mulResult = a * b;
+        question.result = a;
+        a = mulResult;
+      } else {
+        question.result = operator.result(a, b);
+      }
+      question.a = a;
+      question.b = b;
+      updateQuestionText(operator.sign);
+    };
+
+    /**
+     * Skips the current question and creates a new one.
+     */
+    var skip = function () {
+      ++stats.skipped;
+      createNew();
+    };
+
+    /**
+     * Checks whether the input answer is correct and creates a new one if this
+     * is the case.
+     */
+    var verifyAndContinue = function () {
+      var userResult = parseInt($('#result').val());
+      if (!isNaN(userResult) && userResult === question.result) {
+        ++stats.total;
+        createNew();
+      }
+    };
+
+    return {
+      createNew: createNew,
+      skip: skip,
+      verifyAndContinue: verifyAndContinue
+    };
+  }();
+
+  /**
+   * Initializer of the options based on user input.
+   */
   trainer.options = function () {
-    
+
     /**
      * Error functionality.
      */
@@ -124,14 +173,14 @@
         $('.optionerror').removeClass('optionerror');
         errorAdded = false;
       };
-      
+
       return {
         add: add,
         hasErrors: hasErrors,
         reset: reset
       };
     }();
-    
+
     /**
      * Sets the min and max config parameters, ensuring that min is not greater
      * than max.
@@ -149,11 +198,11 @@
       config.min = min;
       config.max = max;
     };
-    
+
     /**
      * Processes the minimum and maximum option fields.
      */
-    var setMinAndMax = function () {
+    var initializeMinAndMax = function () {
       var min = parseInt($('#min').val());
       var max = parseInt($('#max').val());
       if (isNaN(min)) {
@@ -164,11 +213,11 @@
         registerMinAndMaxValues(min, max);
       }
     };
-    
+
     /**
      * Processes timer minutes field.
      */
-    var setMinutes = function () {
+    var initializeMinutes = function () {
       var timerValue = parseInt($('#timer_length').val());
       if (isNaN(timerValue)) {
         error.add('timer_length', 'Please enter a valid number of minutes');
@@ -178,13 +227,13 @@
         config.minutes = timerValue;
       }
     };
-    
+
     /**
      * Processes the operators the user wants to use for the training session.
      * Note that the checkboxes in the HTML are expected to have ID #op_{name},
      * where {name} is the operator abbreviation, e.g. #op_add or #op_div.
      */
-    var setOperators = function () {
+    var initializeOperators = function () {
       var inputOperators = [];
       for (var key in operators) {
         if (operators.hasOwnProperty(key)) {
@@ -199,18 +248,18 @@
         config.operators = inputOperators;
       }
     };
-    
+
     /**
      * Processes the checkbox to avoid negative results for subtraction.
      */
-    var setAvoidNegatives = function () {
+    var initializeAvoidNegatives = function () {
       if (config.min >= 0) {
         config.avoidNegatives = $('#avoid_negative').is(':checked');
       } else {
         config.avoidNegatives = false;
       }
     };
-    
+
     /**
      * Initializes the user options.
      * @returns {Boolean} True if all options are valid and the trainer can be
@@ -218,15 +267,15 @@
      */
     var initialize = function () {
       error.reset();
-      setMinutes();
-      setMinAndMax();
-      setOperators();
+      initializeMinutes();
+      initializeMinAndMax();
+      initializeOperators();
       if (error.hasErrors()) {
         $('#error_wrapper').show();
         return false;
       }
       $('#error_wrapper').hide();
-      setAvoidNegatives();
+      initializeAvoidNegatives();
       return true;
     };
 
@@ -235,90 +284,115 @@
     };
   }();
 
-  // ================
-  // Options <-> Trainer transitions
-  // ================
-  function startTrainer() {
-    stats.total = 0;
-    stats.skipped = 0;
-    $('#options').fadeOut(function() { 
-      $('#question, #user').fadeIn();
-    });
-    $('#score').fadeOut();
-    createNewQuestion();
-    startTimer();
-  }
-  function showScore(timeString) {
-    setScoreText(timeString);
-    $('#user').fadeOut(50, function() {
-      $('#question').fadeOut(function() {
-        $('#score, #options').fadeIn();
-      });
-    });
-  }
-  function setScoreText(timeString) {
-    if (timeString) {
-      $('#score_time').text(timeString);
-    } else {
-      $('#score_time').text(config.minutes + ' minute' + 
-        (config.minutes !== 1 ? 's' : ''));
-    }
-    $('#score_skipped').text(stats.skipped);
-    $('#score_total').text(stats.total);
-  }
-  function startTimer() {
-    $('#timer').countdown(new Date().getTime() + config.minutes * 60000)
-    .on('update.countdown', function (event) {
-      $(this).text(event.strftime('%M:%S'));
-    })
-    // sic: the event is mistyped
-    .on('stoped.countdown', function (event) {
-      // Given minutes = 5, if #timer shows "3:45" we want to
-      // display 1:15 in #score, i.e. {5-3-1}:{60-45}
-      var timeElapsed;
-      if (event.offset.seconds > 0) {
-        timeElapsed = (config.minutes - event.offset.minutes - 1) + ':' +
-          secondsPadding(60 - event.offset.seconds);
+  /**
+   * Initializer and timer functionality.
+   */
+  trainer.run = function () {
+    /**
+     * Sets the score time when the timer has stopped or has been canceled.
+     * @param {?String} timeString Optional string to use as time display if the
+     *  timer was canceled
+     */
+    var setScoreText = function (timeString) {
+      if (timeString) {
+        $('#score_time').text(timeString);
       } else {
-        timeElapsed = (config.minutes - event.offset.minutes) + ':00';
+        $('#score_time').text(config.minutes + ' minute' +
+          (config.minutes !== 1 ? 's' : ''));
       }
-      showScore(timeElapsed);
-    })
-    .on('finish.countdown', function () {
-      showScore();
-    });
-  }
+      $('#score_skipped').text(stats.skipped);
+      $('#score_total').text(stats.total);
+    };
 
+    /**
+     * Shows the score.
+     * @param {?String} timeString Optional string to use as time display if the
+     *  timer was canceled
+     */
+    var showScore = function (timeString) {
+      setScoreText(timeString);
+      $('#user').fadeOut(50, function () {
+        $('#question').fadeOut(function () {
+          $('#score, #options').fadeIn();
+        });
+      });
+    };
 
-  // ================
-  // Helper functions
-  // ================
-  function swapBigger(a, b) {
-    return (a > b) ? [b, a] : [a, b];
-  }
-  function randomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-  function secondsPadding(seconds) {
-    return (seconds >= 10) ? seconds : '0' + seconds;
-  }
+    /** The timer functionality. */
+    var timer = function () {
+      /**
+       * Formats the total elapsed time of the counter if it was stopped
+       * prematurely, e.g. if config.minutes = 5, we return "1:15" if the timer
+       * shows "3:45".
+       * @param {Object} event Event emitted by the timer upon cancellation
+       * @returns {String} The elapsed time as string
+       */
+      var computeElapsedTime = function (event) {
+        if (event.offset.seconds === 0) {
+          return (config.minutes - event.offset.minutes) + ':00';
+        }
+        var remainingSeconds = 60 - event.offset.seconds;
+        return (config.minutes - event.offset.minutes - 1) + ':' +
+          (remainingSeconds < 10 ? '0' : '') + remainingSeconds;
+      };
 
-  // ================
-  // Set event handlers when document is ready
-  // ================
+      /**
+       * Starts the timer and sets up the stopped/finished event.
+       */
+      var start = function () {
+        $('#timer').countdown(new Date().getTime() + config.minutes * 60000)
+        .on('update.countdown', function (event) {
+          $(this).text(event.strftime('%M:%S'));
+        })
+        // sic: the event is mistyped
+        .on('stoped.countdown', function (event) {
+          showScore(computeElapsedTime(event));
+        })
+        .on('finish.countdown', function () {
+          showScore();
+        });
+      };
+
+      return {
+        start: start
+      };
+    }();
+
+    /**
+     * Initializes the start of the trainer and transitions to the trainer view.
+     */
+    var initializeTrainer = function () {
+      stats.total = 0;
+      stats.skipped = 0;
+      $('#options').fadeOut(function () {
+        $('#question, #user').fadeIn();
+      });
+      $('#score').fadeOut();
+      timer.start();
+    };
+
+    return {
+      initializeTrainer: initializeTrainer
+    };
+  }();
+
+  /* ***********
+   * Set event handlers once the document is ready
+   * *********** */
   $(document).ready(function () {
     $('#result').keyup(function (e) {
       if (e.which === 32) {
-        skipQuestion();
+        trainer.question.skip();
       } else {
-        checkUserSubmission();
+        trainer.question.verifyAndContinue();
       }
     });
 
     $('#start').click(function () {
       var hasValidOptions = trainer.options.initialize();
       if (hasValidOptions) {
-        startTrainer();
+        trainer.question.createNew();
+        trainer.run.initializeTrainer();
       }
     });
 
@@ -328,4 +402,4 @@
 
     $('#start').fadeIn();
   });
-}());
+})();
